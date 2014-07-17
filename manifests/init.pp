88ce7dbc1a0f9351a492cfa679cba9e00717f157
Adam Breaux <breauxaj@gmail.com>
2014-07-17 16:41:24 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,33 @@
+# Class: devel
+#
+# This class installs certain development packages
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the list of packages
+#
+# Requires:
+#
+#  EPEL repository
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'devel':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'devel':
+#      ensure => 'absent'
+#    }
+#
 class devel (
   $ensure = 'latest'
 ){
